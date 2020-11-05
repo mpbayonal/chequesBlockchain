f9b980ac107a6a7674f7cd4e0b4015f1847a5731
paolabay <mp.bayonal@uniandes.edu.co>
2020-11-04 22:38:41 -0500

    se hizo un cambio en el processor

--- cheques-blockchain/processor/handler.js
+++ cheques-blockchain/processor/handler.js
@@ -26,6 +26,7 @@ const {
     Record,
     RecordContainer,
     Property,
+    RecordTypeContainer,
     PropertyContainer,
     PropertyPage,
     PropertyValue,
@@ -44,7 +45,6 @@ const {
     SCPayloadActions,
     SCPayloadFields
 } = require('./services/proto')
-
 
 
 /**
@@ -91,7 +91,7 @@ class ChequesHandler extends TransactionHandler {
                     context,
                     signerPublicKey,
                     timestamp,
-                    getActionField(payload,SCPayloadFields.create_record.name)
+                    getActionField(payload, SCPayloadFields.create_record.name)
                 )
                 break
 
@@ -100,7 +100,7 @@ class ChequesHandler extends TransactionHandler {
                     context,
                     signerPublicKey,
                     timestamp,
-                    getActionField(payload,SCPayloadFields.finalize_record.name)
+                    getActionField(payload, SCPayloadFields.finalize_record.name)
                 )
                 break
 
@@ -136,7 +136,7 @@ class ChequesHandler extends TransactionHandler {
                     context,
                     signerPublicKey,
                     timestamp,
-                    getActionField(payload, SCPayloadFields.answer_proposal.name )
+                    getActionField(payload, SCPayloadFields.answer_proposal.name)
                 )
                 break
 
@@ -145,7 +145,7 @@ class ChequesHandler extends TransactionHandler {
                     context,
                     signerPublicKey,
                     timestamp,
-                    getActionField(payload, SCPayloadFields.revoke_reporter.name )
+                    getActionField(payload, SCPayloadFields.revoke_reporter.name)
                 )
                 break
 
@@ -156,8 +156,6 @@ class ChequesHandler extends TransactionHandler {
 
 
 }
-
-
 
 
 async function createAgent(context, signerPublicKey, timestamp, {name}) {
@@ -321,9 +319,6 @@ async function createRecord(
     }
 
 
-
-
-
 }
 
 
@@ -393,7 +388,7 @@ async function finalizeRecord(context, signerPublicKey, timestamp, {record_id}) 
     await context.setState(updates)
 }
 
-async function createProposal(context, signerPublicKey, timestamp, {record_id, receiving_agent,role,  properties}) {
+async function createProposal(context, signerPublicKey, timestamp, {record_id, receiving_agent, role, properties}) {
     const agentAddress = make_agent_address(signerPublicKey)
 
     const state = await context.getState([agentAddress])
@@ -416,7 +411,7 @@ async function createProposal(context, signerPublicKey, timestamp, {record_id, r
 }
 
 
-async function answerProposal(context, signerPublicKey, timestamp, {record_id, receiving_agent,role, response}) {
+async function answerProposal(context, signerPublicKey, timestamp, {record_id, receiving_agent, role, response}) {
     const agentAddress = make_agent_address(signerPublicKey)
 
     const state = await context.getState([agentAddress])
@@ -438,7 +433,7 @@ async function answerProposal(context, signerPublicKey, timestamp, {record_id, r
     await context.setState(updates)
 }
 
-async function revokeReporter(context, signerPublicKey, timestamp, {record_id, reporter_id,properties}) {
+async function revokeReporter(context, signerPublicKey, timestamp, {record_id, reporter_id, properties}) {
     const agentAddress = make_agent_address(signerPublicKey)
 
     const state = await context.getState([agentAddress])
@@ -460,62 +455,246 @@ async function revokeReporter(context, signerPublicKey, timestamp, {record_id, r
     await context.setState(updates)
 }
 
-
-
-
-
-
-
-async function set_property(context, record_id, property_name, property) {
-    const propertyAddress = make_property_address(record_id, property_name, 0)
-
-    const state = await context.getState([propertyAddress])
-
-    // Validation: System Admin is already recorded.
-    if (state[propertyAddress].length > 0) {
-        reject('Ya existe el atributo')
-    }
-
-    let RecordNuevo = Property.create({
-        record_id: record_id,
-        record_type: record_type,
-        final: false,
-
-    })
-
-    // State update.
-    const updates = {}
-
-    updates[agentAddress] = Agent.encode({
-        publicKey: signerPublicKey,
-        timestamp: timestamp,
-        name: name,
-    }).finish()
-
-    await context.setState(updates)
+async function isOwner(record, agent_id) {
+    return record.custodians[record.custodians.length-1].agent_id == agent_id
 }
 
+async function isCustodian(record, agent_id) {
+    return record.custodians[record.custodians.length-1].agent_id == agent_id
+}
 
 async function getRecord(context, record_id) {
-    const recordAddress = make_record_address(record_id)
+    let  recordAddress = make_record_address(record_id)
+    let record_container = getContainer(context, recordAddress, "RECORD")
 
-    const state = await context.getState([recordAddress])
-
-    // Validation: System Admin is already recorded.
-    if (state[agentAddress].length < 0) {
-        reject('Ya existe el usuario')
+    var obj = {
+        container: record_container,
+        address: recordAddress
     }
 
-    // State update.
-    const updates = {}
+    return obj
+}
 
-    updates[agentAddress] = Agent.encode({
-        publicKey: signerPublicKey,
+async function get_record_type(context, type_name) {
+    const type_address = make_record_type_address(type_name)
+
+    let type_container = getContainer(context, type_address, "RECORD_TYPE")
+
+    var obj = {
+        container: type_container,
+        address: type_address
+    }
+
+    return obj
+}
+
+
+
+async function get_property(context, record_id, property_name) {
+    const propertyAddress = make_property_address(record_id, property_name, 0)
+
+    let property_container = getContainer(context, propertyAddress, "PROPERTY")
+
+    var obj = {
+        container: property_container,
+        address: propertyAddress
+    }
+
+    return obj
+}
+
+
+async function set_new_property(context, record_id, property_name, struct_properties, enum_options, fixed, number_exponent, unit, data_type, publickey) {
+    const propertyAddress = make_property_address(record_id, property_name, 0)
+
+
+    let property_container = getContainer(context, propertyAddress, "PROPERTY")
+
+
+    let newProperty = Property.create({
+        signer: publickey,
+        record_id: record_id,
+        data_type: data_type,
+        current_page: 1,
+        wrapped: false,
+        reporters: [],
+        fixed: fixed,
+        number_exponent: number_exponent,
+        unit: unit,
+        enum_options: enum_options,
+        struct_properties: struct_properties
+    })
+
+    newProperty.reporters.push(Property.Reporter.create({
+            public_key: publickey,
+            authorized: true,
+            index: 0
+        }
+    ))
+
+    property_container.entries.push([newProperty])
+
+    setContainer(context, propertyAddress, property_container)
+}
+
+
+async function set_new_propertyPage(context, timestamp, record_id, property_name, value, page_number) {
+    const propertyAddress = make_property_address(record_id, property_name, 0)
+
+
+    let property_container = getContainer(context, propertyAddress, "PROPERTY")
+
+    let page = PropertyPage.create({
+        name: property_name,
+        record_id: record_id,
+        reported_values: []
+    })
+
+
+    if (value) {
+
+        page.reported_values.push()
+    }
+
+
+    property_container.entries.push([page])
+
+    setContainer(context, propertyAddress, property_container)
+}
+
+async function make_new_reported_value(reporter_index, timestamp, prop) {
+
+    let reported_value = PropertyPage.ReportedValue.create({
+        reporter_index: reporter_index,
         timestamp: timestamp,
-        name: name,
-    }).finish()
+    })
+
+
+    var DATA_TYPE_TO_ATTRIBUTE = {}
+
+    DATA_TYPE_TO_ATTRIBUTE[PropertySchema.BYTES] = 'bytes_value'
+    DATA_TYPE_TO_ATTRIBUTE[PropertySchema.STRING] = 'string_value'
+    DATA_TYPE_TO_ATTRIBUTE[PropertySchema.INT] = 'int_value'
+    DATA_TYPE_TO_ATTRIBUTE[PropertySchema.FLOAT] = 'float_value'
+
+
+    switch (prop.data_type) {
+        case PropertySchema.BYTES:
+            reported_value.bytes_value = prop.bytes_value;
+            break;
+        case PropertySchema.BOOLEAN:
+
+            reported_value.boolean_value = prop.boolean_value;
+
+            break;
+        case PropertySchema.NUMBER:
+
+            reported_value.boolean_value = prop.number_value;
+
+            break;
+        case PropertySchema.STRING:
+
+            reported_value.boolean_value = prop.string_value;
+
+            break;
+        case PropertySchema.ENUM:
+            reported_value.bytes_value = prop.enum_value;
+            break;
+
+
+        default:
+            let text = "I have never heard of that fruit...";
+    }
+
+
+
+
+    return reported_value
+
+
+
+
+}
+
+
+async function getContainer(context, address, entity) {
+
+    let container = null
+    switch (entity) {
+        case "AGENT":
+            container = AgentContainer;
+            break;
+        case "PROPERTY":
+            if (address.substr(-4) === "0000") {
+                container = PropertyContainer;
+            } else {
+                container = PropertyPageContainer;
+            }
+
+            break;
+        case "PROPOSAL":
+            container = ProposalContainer;
+            break;
+        case "RECORD":
+            container = RecordContainer;
+            break;
+        case "RECORD_TYPE":
+            container = RecordTypeContainer;
+            break;
+
+        default:
+            let text = "I have never heard of that fruit...";
+    }
+
+    const entries = await context.getState([address])
+    console.log(entries)
+
+    let data = null
+    if (entries !== null) {
+        data = entries[0].data
+
+
+    }
+
+    return container.decode(data)
+}
+
+async function setContainer(context, address, container, entity) {
+
+
+    const updates = {}
+    let entityContainer = null
+
+    switch (entity) {
+        case "AGENT":
+            entityContainer = Agent;
+            break;
+        case "PROPERTY":
+
+            entityContainer = Property;
+
+            break;
+        case "PROPOSAL":
+            entityContainer = Proposal;
+            break;
+        case "RECORD":
+            entityContainer = Record;
+            break;
+        case "RECORD_TYPE":
+            entityContainer = RecordType;
+            break;
+
+        default:
+            let text = "I have never heard of that fruit...";
+    }
+
+    // Record field.
+    updates[address] = entityContainer.encode(container).finish()
 
     await context.setState(updates)
+
+
 }
+
 
 module.exports = ChequesHandler
