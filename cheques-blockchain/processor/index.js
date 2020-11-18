3a87b81643d983b5d1729deef7494e6a0c77db8e
lef <le.florez602@uniandes.edu.co>
2020-11-18 11:37:40 -0500

    works in minikube

--- cheques-blockchain/processor/index.js
+++ cheques-blockchain/processor/index.js
@@ -3,7 +3,7 @@ const {TransactionProcessor} = require('sawtooth-sdk/processor')
 const ChequesHandler = require('./handler')
 
 
-const tp = new TransactionProcessor(process.env.VALIDATOR_URL )
+const tp = new TransactionProcessor(process.env.VALIDATOR )
 
 
 const handler = new ChequesHandler()
---
+++ k8s/README.md
@@ -0,0 +1,5 @@
+kubectl exec -it pod/myapp-deployment-69f9c7cc58-6nnsh bash
+
+npm run seed-example-users
+
+curl http://192.168.99.100:30001/agents
